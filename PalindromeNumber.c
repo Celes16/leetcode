b06794ac3438f86f2fa5a20bfa66ae5257cabd6e
Maria Celeste <155506763+Celes16@users.noreply.github.com>
2024-12-02 00:21:24 +0000

    leetcode

---
+++ PalindromeNumber.c
@@ -0,0 +1,37 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+bool isPalindrome(int x) {
+
+    if (x < 0 || (x % 10 == 0 && x != 0)) {
+        return false;
+    }
+
+    int reversed = 0;
+    int original = x;
+
+    while (x > 0) {
+        int digit = x % 10;
+        reversed = reversed * 10 + digit;
+        x /= 10;
+    }
+
+
+    return original == reversed;
+}
+
+
+int main() {
+    int num;
+
+    printf("Digite um número: ");
+    scanf("%d", &num);
+
+    if (isPalindrome(num)) {
+        printf("%d é um número palíndromo.\n", num);
+    } else {
+        printf("%d não é um número palíndromo.\n", num);
+    }
+
+    return 0;
+}
