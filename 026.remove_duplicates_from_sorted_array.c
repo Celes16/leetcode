cb9d48a539dc4c83a0497ee5949868eeee92561d
Maria Celeste <155506763+Celes16@users.noreply.github.com>
2025-01-10 01:32:13 -0300

    updates

--- 026.remove_duplicates_from_sorted_array.c
+++ 026.remove_duplicates_from_sorted_array.c
@@ -1,32 +1,38 @@
 #include <stdio.h>
 
-int removeDuplicates(int* nums, int numsSize) {
-    if (numsSize == 0) return 0;
+int removeDuplicates (int* nums, int numsSize){
+    if (numsSize == 0) { 
+        return 0;
+    }
 
-    int uniqueIndex = 0; 
+   int c = 1;
+   for (int i = 0; i < numsSize; i++){
+    if (nums [i] != nums [c - 1]){
+        nums [c] = nums [i]; //ele vai colocar o elemento diferente (que não se repete) na posição do seguinte numero unico que teremos no array que é o numero de endereço i que foi diferente nas comparações. exemplo: [1,1,1,3] ele vai modificar para [1,3,1,3] e o loop terminaria ai porque o i teria chegado ao ultimo endereço de comparação e o contador teria encerrado.
+        c++;
+    }
+   }
+   return c;
+}
 
-    for (int i = 1; i < numsSize; i++) {
-        if (nums[i] != nums[uniqueIndex]) { 
-            uniqueIndex++;        
-            nums[uniqueIndex] = nums[i]; 
+int main(){
+    int nums[] = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+    int numsSize = sizeof (nums) / sizeof (nums[0]);
+
+    int uniqueCount = removeDuplicates (nums, numsSize);
+
+    printf("%d, nums=[", uniqueCount);
+    for (int i = 0; i < numsSize; i++){
+        if (i < uniqueCount){
+            printf ("%d", nums[i]);
+        } else {
+            printf("_");
+        }
+        if (i < numsSize - 1){
+            printf (", "); 
         }
     }
 
-    return uniqueIndex + 1; 
-}
-
-int main() {
-    int nums[] = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
-    int numsSize = sizeof(nums) / sizeof(nums[0]);
-
-    int newSize = removeDuplicates(nums, numsSize);
-
-    printf("Novo comprimento: %d\n", newSize);
-    printf("Array sem duplicatas: ");
-    for (int i = 0; i < newSize; i++) {
-        printf("%d ", nums[i]);
-    }
-    printf("\n");
-
+    printf("]");
     return 0;
 }
